9ef27e5540574d378773500b5adc4698c40dee83
ktalimaa <kristeltalimaa@gmail.com>
2022-10-20 20:06:25 +0300

    AssertJ examples added

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -2,10 +2,24 @@ package org.example.calculator;
 
 public class Calculator {
     public float addArray(float[] inputArray) {
-        int result = 0;
+        float result = 0;
 
         for (float input : inputArray) {
             result += input;
+        }
+        return result;
+    }
+
+    public float addNumbers(int a, int b) {
+        return a + b;
+    }
+
+
+    public float multiplyArray(float[] inputArray) {
+        float result = inputArray[0];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            result *= inputArray[i];
         }
         return result;
     }
