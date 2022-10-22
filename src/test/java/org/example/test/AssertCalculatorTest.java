11d439980a1ad2a26b7a2d5cb53c39336ba5e7ca
ktalimaa <kristeltalimaa@gmail.com>
2022-10-22 16:01:41 +0300

    AssertJ homework started

--- src/test/java/org/example/test/AssertCalculatorTest.java
+++ src/test/java/org/example/test/AssertCalculatorTest.java
@@ -1,7 +1,12 @@
 package org.example.test;
 
+import org.assertj.core.api.Assertions;
 import org.example.calculator.Calculator;
+import org.example.calculator.Divide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 /**
  * Write some AssertJ tests for Calculator methods in a separate file 'AssertCalculatorTest'.
@@ -9,5 +14,17 @@ import org.junit.jupiter.api.Test;
  * @author Kristel Talimaa
  */
 public class AssertCalculatorTest {
+
+    public DivideTest divideTest;
+
+    @Test
+    public void testDivideList() {
+        List<Integer> divide = List.of(10000, 2);
+
+        Assertions.assertThat(divide).hasSize(2)
+                .contains(2)
+                .startsWith(10000);
+
+    }
 
 }
