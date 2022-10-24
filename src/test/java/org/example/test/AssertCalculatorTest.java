c81eff590498ae48cafbf0d561de9a69d901c332
ktalimaa <kristeltalimaa@gmail.com>
2022-10-24 19:53:06 +0300

    AssertJ homework done

--- src/test/java/org/example/test/AssertCalculatorTest.java
+++ src/test/java/org/example/test/AssertCalculatorTest.java
@@ -24,7 +24,40 @@ public class AssertCalculatorTest {
         Assertions.assertThat(divide).hasSize(2)
                 .contains(2)
                 .startsWith(10000);
+    }
 
+    public MultiplyTest multiplyTest;
+
+    @Test
+    public void testMultiplyTest() {
+        List<Integer> divide = List.of(20, 3, 5);
+
+        Assertions.assertThat(divide).hasSize(3)
+                .contains(3)
+                .startsWith(20);
+    }
+
+
+    public SubtractTest subtractTest;
+
+    @Test
+    public void testSubtractTest() {
+        List<Integer> divide = List.of(50, 3, 4);
+
+        Assertions.assertThat(divide).hasSize(3)
+                .contains(50)
+                .startsWith(50);
+    }
+
+    public CalculatorTest calculatorTest;
+
+    @Test
+    public void testCalculatorTest() {
+        List<Integer> divide = List.of(10, 1, 2);
+
+        Assertions.assertThat(divide).hasSize(3)
+                .contains(10)
+                .startsWith(10);
     }
 
 }
